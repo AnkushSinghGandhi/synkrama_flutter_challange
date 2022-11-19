0315a768f1b3b4cacbe66fac200d982b4026ffd6
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2022-11-19 16:36:21 +0530

    made login screen

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,169 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginDemo(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class LoginDemo extends StatefulWidget {
+  const LoginDemo({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<LoginDemo> createState() => _LoginDemoState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _LoginDemoState extends State<LoginDemo> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Stack(
+      // <-- STACK AS THE SCAFFOLD PARENT
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/bg.png"), // <-- BACKGROUND IMAGE
+              fit: BoxFit.cover,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Scaffold(
+          backgroundColor:
+              Colors.transparent, // <-- SCAFFOLD WITH TRANSPARENT BG
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                const SizedBox(
+                  height: 70,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 60.0),
+                  child: Center(
+                    // ignore: sized_box_for_whitespace
+                    child: Container(
+                      width: 200,
+                      height: 150,
+                      /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                      child: const Icon(
+                        Icons.person_add,
+                        color: Colors.white,
+                        size: 84.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 2, horizontal: 60),
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.person,
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 230.0,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Email Id',
+                              hintText: 'enter your email id',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              labelStyle: TextStyle(
+                                fontSize: 15.0,
+                                color: Color.fromARGB(166, 255, 255, 255),
+                                height: 1,
+                              ),
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(166, 255, 255, 255),
+                              ),
+                              contentPadding: EdgeInsets.only(
+                                bottom: 0, // HERE THE IMPORTANT PART
+                              )),
+                          style: TextStyle(
+                              fontSize: 12.0, height: 1, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 2, horizontal: 60),
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                        size: 24.0,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 230.0,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              labelText: 'Password',
+                              hintText: 'enter your password',
+                              enabledBorder: UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              labelStyle: TextStyle(
+                                fontSize: 15.0,
+                                color: Color.fromARGB(166, 255, 255, 255),
+                                height: 1,
+                              ),
+                              hintStyle: TextStyle(
+                                color: Color.fromARGB(166, 255, 255, 255),
+                              ),
+                              contentPadding: EdgeInsets.only(
+                                bottom: 0, // HERE THE IMPORTANT PART
+                              )),
+                          style: TextStyle(
+                              fontSize: 12.0, height: 1, color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 80,
+                ),
+                Container(
+                  height: 40,
+                  width: 200,
+                  decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(50)),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'Login',
+                      style: TextStyle(color: Colors.white, fontSize: 18),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
