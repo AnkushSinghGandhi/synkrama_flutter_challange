c4e290bdad3740aea47ecebaaf3a3c3eb7d3ee84
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2022-11-20 17:42:09 +0530

    removed unused import

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:synkrama/firebase_options.dart';
 import 'package:synkrama/constants/routes.dart';
 import 'package:firebase_core/firebase_core.dart';
 
