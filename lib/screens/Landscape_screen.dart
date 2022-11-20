0a353efe8d0b139ce10e952c3a77c05552f24fa4
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2022-11-20 18:28:48 +0530

    added landscapeview

---
+++ lib/screens/Landscape_screen.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:video_player/video_player.dart';
+import 'package:synkrama/constants/routes.dart';
+
+class LandscapeView extends StatefulWidget {
+  const LandscapeView({super.key});
+
+  @override
+  State<LandscapeView> createState() => _LandscapeViewState();
+}
+
+class _LandscapeViewState extends State<LandscapeView> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset('assets/videoplayback.mp4')
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => controller.play());
+
+    setLandscape();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+              child: Column(
+            children: [
+              VideoPlayerWidget(controller: controller),
+            ],
+          )),
+        ),
+        Positioned(
+          top: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: AppBar(
+            leading: IconButton(
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 47,
+                ),
+                onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    homeRoute,
+                  );
+                }),
+            backgroundColor: Colors.blue.withOpacity(0.3),
+            elevation: 0.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Future setLandscape() async {
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.landscapeLeft,
+    DeviceOrientation.landscapeRight,
+  ]);
+}
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+  const VideoPlayerWidget({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unnecessary_null_comparison
+    return controller != null && controller.value.isInitialized
+        ? Container(
+            alignment: Alignment.topCenter,
+            child: buildVideo(),
+          )
+        :
+        // ignore: sized_box_for_whitespace, dead_code
+        Container(
+            height: 200,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+  }
+
+  Widget buildVideo() => AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller),
+      );
+}
