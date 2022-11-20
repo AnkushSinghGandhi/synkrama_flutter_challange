209d85ee52f19b174423654a8142090cd065db8c
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2022-11-20 09:26:36 +0530

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:synkrama/screens/home_screen.dart';
 import 'package:synkrama/screens/signup_screen.dart';
 import 'package:synkrama/screens/login_screen.dart';
+import 'package:synkrama/constants/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +13,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginDemo(),
+      home: const SignupDemo(),
+      routes: {
+        loginRoute: (context) => const LoginDemo(),
+        signupRoute: (context) => const SignupDemo(),
+        homeRoute: (context) => const HomeScreen(),
+      },
     );
   }
 }
