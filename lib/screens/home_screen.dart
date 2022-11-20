69c239aee62954b172aa19ab862559cb1066a02b
Ankush Singh Gandhi <ankushsinghgandhi@gmail.com>
2022-11-20 18:27:30 +0530

    added video player

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:synkrama/constants/routes.dart';
+import 'package:video_player/video_player.dart';
 
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late VideoPlayerController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset('assets/videoplayback.mp4')
+      ..addListener(() => setState(() {}))
+      ..setLooping(true)
+      ..initialize().then((_) => controller.play());
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/bg.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SingleChildScrollView(
+              child: Column(
+            children: [
+              VideoPlayerWidget(controller: controller),
+              Container(
+                height: 40,
+                width: 200,
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(50)),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Next',
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ),
+        Positioned(
+          top: 0.0,
+          left: 0.0,
+          right: 0.0,
+          child: AppBar(
+            leading: IconButton(
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+                size: 47,
+              ),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            backgroundColor: Colors.blue.withOpacity(0.3),
+            elevation: 0.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+  const VideoPlayerWidget({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: unnecessary_null_comparison
+    return controller != null && controller.value.isInitialized
+        ? Container(
+            alignment: Alignment.topCenter,
+            child: buildVideo(),
+          )
+        :
+        // ignore: sized_box_for_whitespace, dead_code
+        Container(
+            height: 200,
+            child: const Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+  }
+
+  Widget buildVideo() => AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: VideoPlayer(controller),
+      );
+}
